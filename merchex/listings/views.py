b98ae4e1d142822c7dedb1f59615db39a6432420
Si-m0n <simondefradas@gmail.com>
2022-12-24 18:00:23 +0100

    Added templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -7,34 +7,27 @@ from listings.models import Listing
 # Create your views here.
 def hello(request):
     band = Band.objects.all()
-    return HttpResponse(
-        f"""<h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :</p>
-        <ul>
-            <li>{band[0].name}</li>
-            <li>{band[1].name}</li>
-            <li>{band[2].name}</li>
-        </ul>
-    """
+    return render(
+        request,
+        "listings/hello.html",
+        {"bands": band},
     )
 
 
 def about(request):
-    return HttpResponse("<h1>À propos</h1> <p>Nous adorons merch !</p>")
+    return render(request, "listings/about.html")
 
 
 def listings(request):
-    listing = Listing.objects.all()
-    return HttpResponse(
-        f"""<h1>Listings</h1>
-        <ul>
-            <li>{listing[0].title}</li>
-            <li>{listing[1].title}</li>
-            <li>{listing[2].title}</li>
-            <li>{listing[3].title}</li>
-    """
+    listings = Listing.objects.all()
+    return render(
+        request,
+        "listings/listings.html",
+        {
+            "listings": listings,
+        },
     )
 
 
 def contact(request):
-    return HttpResponse("<h1>Nous contacter :</h1> <p>Téléphone : 06 ** ** ** ** </p>")
+    return render(request, "listings/contact.html")
